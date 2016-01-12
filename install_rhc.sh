dc6c3a8956f898e767eb1074ad4362f875900152
Thiago FOnseca <dev.thiago@gmail.com>
2016-01-12 21:06:09 -0200

    Inserting new filed

---
+++ install_rhc.sh
@@ -0,0 +1,9 @@
+# Install Ruby
+sudo apt-get install ruby-full
+
+#Install Ruby gems
+sudo apt-get install rubygems
+
+#Install RHC
+sudo gem install rhc
+
---
+++ linux_commands.sh
@@ -0,0 +1,17 @@
+echo 'Disk command'
+
+df -H
+
+echo 'Memory command'
+
+free -m
+
+echo 'top Command'
+
+# Intervalo em segundos
+top -d 1
+
+# C - Show full command path
+# K - KIll a process
+
+
