0ff434a9ab3039090292cd2cd163467f85dd9bb5
Thiago FOnseca <dev.thiago@gmail.com>
2016-11-05 12:52:04 -0200

    Adding docker commands

--- docker_commands.sh
+++ docker_commands.sh
@@ -1,1 +1,8 @@
 docker inspect --format '{{ .NetworkSettings.IPAddress }}' gestao_cervejeira
+
+# POSTGRES
+docker run --name gestao_cervejeira -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=gestao_cervejeira -d postgres
+
+# PG-ADMIN
+docker run --name pg-admin --link gestao_cervejeira:postgres -p 5050:5050 -d fenglc/pgadmin4
+
