b0036ff195337713869b92bbd43230324224b193
Thiago Santos Fonseca <tfonseca@ciandt.com>
2017-03-19 16:16:23 -0300

    [tfonseca][CIT][ms] Teste do commit

--- shell/gitCommands.sh
+++ shell/gitCommands.sh
@@ -12,3 +12,9 @@ git apply --check $file
 
 # Aplica o patch efetivamente
 git am < $file
+
+# git alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
