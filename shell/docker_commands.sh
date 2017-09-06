d04f2388529a4abf907c76d723f8b00a4b926fd5
Thiago Fonseca <dev.thiago@gmail.com>
2017-09-06 13:04:52 -0300

    Update docker_commands.sh

--- shell/docker_commands.sh
+++ shell/docker_commands.sh
@@ -6,3 +6,9 @@ docker run --name gestao_cervejeira -e POSTGRES_PASSWORD=pass -e POSTGRES_DB=ges
 # PG-ADMIN
 docker run --name pg-admin --link gestao_cervejeira:postgres -p 5050:5050 -d fenglc/pgadmin4
 
+# Construir container, através de um DOCKERFILE
+docker build -t json-schema-image .
+
+# Inicia o container
+docker run -p 8081:8081 json-schema-image
+
