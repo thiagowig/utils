fe04ff61b7c7b10ae6131ac27629cc397dfb9dec
Thiago Fonseca <dev.thiago@gmail.com>
2019-03-24 16:18:34 -0300

    Creating the docker-compose file

--- docker/jenkins/commands.sh
+++ docker/jenkins/commands.sh
@@ -1,6 +1,6 @@
 docker pull jenkins/jenkins:lts
 
-docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home --name jenkins_tutorial jenkins/jenkins:lts
+docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home --name jenkins jenkins/jenkins:lts
 
 #Initial Key
 3f3143fa929f4313b47a59200b1cb978
