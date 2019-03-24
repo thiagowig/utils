6860f079d55b0282fd55b717f74b66cc93f2d4c5
Thiago Fonseca <dev.thiago@gmail.com>
2019-03-24 14:40:23 -0300

    Including Jenkins in docker

--- docker/jenkins/commands.sh
+++ docker/jenkins/commands.sh
@@ -1,3 +1,5 @@
 docker pull jenkins/jenkins:lts
 
 docker run -p 8080:8080 -p 50000:50000 -v jenkins_home:/var/jenkins_home --name jenkins_tutorial jenkins/jenkins:lts
+
+3f3143fa929f4313b47a59200b1cb978
