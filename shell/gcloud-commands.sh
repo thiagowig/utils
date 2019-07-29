a61e38c30dff3507d62764f9ddc1b7af596e089f
Thiago Fonseca <dev.thiago@gmail.com>
2019-07-28 21:20:09 -0300

    Update gcloud-commands.sh

--- shell/gcloud-commands.sh
+++ shell/gcloud-commands.sh
@@ -4,5 +4,8 @@ gcloud config set project my-project
 # Deploy a function
 gcloud funtions deploy <FUNC_NAME> --entry-point <METHOD_NAME> --runtime nodejs6 --trigger-http
 
-# Create a Pubsub
+# Create a Pubsub topic
 gcloud pubsub topics create <PUBSUB_NAME>
+
+# Create a Pubsub subscription
+gcloud pubsub subscriptions create <SUBSCRIPTION_NAME> --topic <TOPIC_NAME>
