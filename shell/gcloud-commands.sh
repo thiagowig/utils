821bd7d69a9aed683bda72e05d52aae9e79db1a5
Thiago Fonseca <dev.thiago@gmail.com>
2019-09-13 09:14:30 -0300

    Update gcloud-commands.sh

--- shell/gcloud-commands.sh
+++ shell/gcloud-commands.sh
@@ -1,8 +1,12 @@
-# Login
+# LOGIN
 gcloud auth login
+gcloud auth revoke
 
 # Change project
 gcloud config set project my-project
+
+
+# FUNCTIONS
 
 # Deploy a function
 gcloud funtions deploy <FUNC_NAME> --entry-point <METHOD_NAME> --runtime nodejs6 --trigger-http
@@ -10,11 +14,20 @@ gcloud funtions deploy <FUNC_NAME> --entry-point <METHOD_NAME> --runtime nodejs6
 # Read function log
 gcloud functions logs read <FUNC_NAME>
 
+
+# PUBSUB
+
 # Create a Pubsub topic
 gcloud pubsub topics create <PUBSUB_NAME>
 
 # Create a Pubsub subscription
 gcloud pubsub subscriptions create <SUBSCRIPTION_NAME> --topic <TOPIC_NAME>
+
+
+#CLOUD SQL
+
+# List all instances
+gcloud sql instances list
 
 # Create a Cloud SQL instance
 gcloud sql instances create poc-instance --tier=db-f1-micro --region=us-central
@@ -22,7 +35,8 @@ gcloud sql instances create poc-instance --tier=db-f1-micro --region=us-central
 # Create a user um CloudSQL
 gcloud sql users set-password root --host=% --instance [INSTANCE_NAME] --password [PASSWORD]
 
-# Redis
+
+# REDIS
 
 ## Create instance
 gcloud redis instances create myinstance --size=2 --region=us-central1 --redis-version=redis_4_0
